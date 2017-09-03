d8e2f7568cca99c90c54865f74e08638fa79ea71
Knut Andreas Kyrvestad <knut1263@yahoo.com>
2017-09-03 20:23:33 +0200

    final commit oppgave1

--- testApps/argArray.c
+++ testApps/argArray.c
@@ -2,8 +2,9 @@
 int argCheck(int msg, char **param);
 
 int main(int argc, char *argv[]) {
-  if ( argc == 2 ) {
+  if (argc == 2) {
     int x = argCheck(argc, argv);
+    printf("In Main: Number was: %i \n", x);
   } else {
     printf("arguments not correct: programName argumentOne \n");
   }
@@ -15,14 +16,14 @@ int argCheck(int msg, char **param) {
   int n;
   char ch;
   
-  for(i=0; i<msg; i++) {
+  for(i=1; i<msg; i++) {
     n = sscanf(param[i], "%d%c", &value, &ch);
     if (n != 1) {
       printf("not a number\n");
     } else {
-      //int x = value;
-      printf("Input was number: %i \n", value);
+     int x = value;
+     printf("input was a number: %i \n", x);
+     return x;
     }
   }
-  return 0;
 }
